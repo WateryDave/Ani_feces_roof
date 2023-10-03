16e2b1999db72add7e52b2973781f72059ebfd7a
WateryDave <89871434+WateryDave@users.noreply.github.com>
2023-10-03 14:10:12 -0400

    Update Animal_Names.R

--- Animal_Names.R
+++ Animal_Names.R
@@ -2,7 +2,10 @@
 
 setwd("C:/Users/DDEMAREE/OneDrive - Environmental Protection Agency (EPA)/FI to Pathogen RainRWH/R Stuff/Name experiment")
 
+# File with animal Taxonomy
 Taxonomy <- read.csv("Animal Taxonomy DHD_J-EPC0033278-QP-1-0_20230407v03.csv")
+
+Input files
 RawData <- read.csv("Salmonella.csv")
 RawData <- read.csv("Cryptosporidium.csv")
 RawData <- read.csv("Giardia.csv")
@@ -31,3 +34,6 @@ for (j in 1:b) {
 # This works as a fist pass there are still a bunch of places that need fixing
 
 write.csv(RawData, "AddedAnimalNamesCampy.csv", row.names = FALSE)
+write.csv(RawData, "AddedAnimalNamesCryp.csv", row.names = FALSE)
+write.csv(RawData, "AddedAnimalNamesGia.csv", row.names = FALSE)
+write.csv(RawData, "AddedAnimalNamesSal.csv", row.names = FALSE)
